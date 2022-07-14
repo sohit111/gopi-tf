eef325591b863dde808a5ae1a55a2cf7e48920a5
sohit111 <108972903+sohit111@users.noreply.github.com>
2022-07-14 15:31:47 +0530

    Initial commit

---
+++ variable.tf
@@ -0,0 +1,12 @@
+variable "region" {
+    default = "us-east-2" #ohio  
+}
+
+variable "ami" {
+    defdefault = "ami-02f3416038bdb17fb"  
+}
+
+variable "instance_type" {
+    instance_type = "t2.micro"
+  
+}
